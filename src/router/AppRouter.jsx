2b4988d5143be1b6c8e3b8aab3a1259efe518ea8
Carlos Vilchez Malasquez <carlosvmp0@gmail.com>
2022-07-06 00:24:50 -0500

    finish project

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { Route, HashRouter as Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 
 import { LoginPage } from "../auth"
 import { PublicRoute } from "./PublicRoute"
