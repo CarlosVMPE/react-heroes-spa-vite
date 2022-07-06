2e97f463c7cfb6844bef84aae0b7454388a61f65
Carlos Vilchez Malasquez <carlosvmp0@gmail.com>
2022-07-05 22:11:15 -0500

    adding searching page for heroes

---
+++ src/hooks/useForm.js
@@ -0,0 +1,41 @@
+
+
+import { useState } from "react";
+
+/* Custom Form Hook */
+/* 
+  Set an instance, you need to pass the values for your form:
+  const { description, onInputChange, onResetForm } = useForm({
+        description: ''
+    });
+*/
+/*
+  If you can use it, you need to set a tag "name" to your input
+  eg:
+  <input type="text" placeholder="abc"
+                name="description"
+                value={description}
+                onChange={onInputChange} />
+*/
+export const useForm = (initialForm = {}) => {
+    const [formState, setFormState] = useState(initialForm);
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const onResetForm = () => {
+        setFormState(initialForm);
+    }
+
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm
+    };
+};
