e18d98509874bfbf2cb6c67db7d86c430d2195b7
Carlos Vilchez Malasquez <carlosvmp0@gmail.com>
2022-07-06 00:18:24 -0500

    updating vite conf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: '/react-heroes-spa-vite',
+  base: '/react-heroes-spa-vite',
   plugins: [react()]
 })
